da99ed487fc012de8305d68075ed25f5bebcfcbd
djlimdiwala <dhaval.limdiwala@gmail.com>
2018-03-05 03:36:24 +0530

    File handling done

--- app/src/main/java/com/example/lkh/a173050061_record_data/accelerometer.java
+++ app/src/main/java/com/example/lkh/a173050061_record_data/accelerometer.java
@@ -7,10 +7,23 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.Environment;
 import android.os.Handler;
+import android.content.SharedPreferences;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.util.Log;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static java.security.AccessController.getContext;
 
 /**
  * Created by DJL on 3/4/2018.
@@ -23,9 +36,14 @@ public class accelerometer extends Service implements SensorEventListener {
     private Sensor senAccelerometer;
     private long lastUpdate = 0;
     private float last_x, last_y, last_z;
+    private String label = "walking";
+    private double latitude, longitude;
     private static final int SHAKE_THRESHOLD = 600;
 
 
+
+    String csv = android.os.Environment.getExternalStorageDirectory().getAbsolutePath();
+    String path = csv + File.separator + "sample.csv";
 
 
     @Nullable
@@ -39,6 +57,27 @@ public class accelerometer extends Service implements SensorEventListener {
         senSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         senAccelerometer = senSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         senSensorManager.registerListener(this, senAccelerometer, SensorManager.SENSOR_DELAY_UI, new Handler());
+
+
+
+                label = intent.getExtras().get("label").toString();
+                String str = intent.getExtras().get("first") + "," + intent.getExtras().get("last") + "," + intent.getExtras().get("mobile") + "," + intent.getExtras().get("email") + "," + intent.getExtras().get("gend") + "," + intent.getExtras().get("age") + "\n";
+
+                Log.e("path",path);
+                try {
+                    FileOutputStream stream = new FileOutputStream(path);
+                    stream.write(str.getBytes());
+                    str = "Timestamp,Lat,Long,Accelx,Accely,Accelz,Label\n";
+                    stream.write(str.getBytes());
+                    stream.close();
+
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+
         return START_STICKY;
     }
 
@@ -54,6 +93,22 @@ public class accelerometer extends Service implements SensorEventListener {
             last_x = x;
             last_y = y;
             last_z = z;
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MM-yyyy-hh-mm-ss");
+            String format = simpleDateFormat.format(new Date());
+            String str = format + "," + String.valueOf(latitude) + "," + String.valueOf(longitude) + "," + String.valueOf(last_x) + "," + String.valueOf(last_y) + "," + String.valueOf(last_z) + "," + label + "\n";
+
+
+            try {
+                FileOutputStream stream = new FileOutputStream(path, true);
+                stream.write(str.getBytes());
+                stream.close();
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
             String read = String.valueOf(last_x) + "  " + String.valueOf(last_y) + "  " + String.valueOf(last_z);
 //            Log.e("recording",read);
         }
--- app/src/main/java/com/example/lkh/a173050061_record_data/fragment_record.java
+++ app/src/main/java/com/example/lkh/a173050061_record_data/fragment_record.java
@@ -9,6 +9,7 @@ package com.example.lkh.a173050061_record_data;
 import android.content.Context;
 import android.content.ContextWrapper;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -23,6 +24,8 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.CompoundButton;
 import android.widget.ListView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Switch;
 import android.hardware.SensorEventListener;
 
@@ -40,28 +43,30 @@ import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
-public class fragment_record extends Fragment implements SensorEventListener {
+public class fragment_record extends Fragment {
 
 
+    SharedPreferences preferences;
     private SensorManager senSensorManager;
     private Sensor senAccelerometer;
     private Sensor senGPS;
     private long lastUpdate = 0;
     private float last_x, last_y, last_z;
     private float longitude, latitude;
+    private RadioGroup rrg;
+    private RadioButton rbt;
     private static final int SHAKE_THRESHOLD = 600;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_record, container, false);
+        final View rootView = inflater.inflate(R.layout.fragment_record, container, false);
 
 
-        senSensorManager = (SensorManager) getActivity().getSystemService(Context.SENSOR_SERVICE);
-        senAccelerometer = senSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 
-
+        preferences = this.getActivity().getSharedPreferences("myAppPrefs", getContext().MODE_PRIVATE);
+        final SharedPreferences.Editor editor = preferences.edit();
 
 
 
@@ -88,29 +93,50 @@ public class fragment_record extends Fragment implements SensorEventListener {
 
                 SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MM-yyyy-hh-mm-ss");
                 String format = simpleDateFormat.format(new Date());
-                String csv = android.os.Environment.getExternalStorageDirectory().getAbsolutePath();
-                String path = csv + File.separator + "sample.csv";
-                String str = "first name,last name,mobile, email,gender,age";
+//                String csv = android.os.Environment.getExternalStorageDirectory().getAbsolutePath();
+//                String path = csv + File.separator + "sample.csv";
+//                String str = "first name,last name,mobile, email,gender,age";
+//
+//                Log.e("path",path);
+//                try {
+//                    FileOutputStream stream = new FileOutputStream(path);
+//                    stream.write(str.getBytes());
+//                    stream.close();
+//
+//                } catch (FileNotFoundException e) {
+//                    e.printStackTrace();
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
 
-                Log.e("path",path);
-                try {
-                    FileOutputStream stream = new FileOutputStream(path);
-                    stream.write(str.getBytes());
-                    stream.close();
 
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
-
+                Intent intent = new Intent(getActivity(),accelerometer.class);
                 if (isChecked)
                 {
+                    rrg = (RadioGroup) rootView.findViewById(R.id.record_rg);
+                    int selectedId = rrg.getCheckedRadioButtonId();
+                    rbt = (RadioButton) rootView.findViewById(selectedId);
+                    Log.e("radio", rbt.getText().toString());
+                    int g = preferences.getInt("gender_check",-1);
+                    String gc;
+                    if (g == 1)
+                    {
+                        gc = "Female";
+                    }
+                    else
+                    {
+                        gc = "Male";
+                    }
 
-                    getActivity().startService(new Intent(getActivity(),accelerometer.class));
+                    intent.putExtra("first",preferences.getString("first_check",""));
+                    intent.putExtra("last",preferences.getString("last_check",""));
+                    intent.putExtra("mobile",preferences.getString("mobile_check",""));
+                    intent.putExtra("email",preferences.getString("mail_check",""));
+                    intent.putExtra("gend",gc);
+                    intent.putExtra("age",preferences.getString("age_check",""));
+                    intent.putExtra("label",rbt.getText().toString());
 
-//                    senSensorManager.registerListener(fragment_record.this, senAccelerometer , SensorManager.SENSOR_DELAY_NORMAL);
+                    getActivity().startService(intent);
 
                     if (recordings_list.size() >= 5)
                     {
@@ -123,8 +149,7 @@ public class fragment_record extends Fragment implements SensorEventListener {
                 }
                 else
                 {
-                    getActivity().stopService(new Intent(getActivity(),accelerometer.class));
-                    senSensorManager.unregisterListener(fragment_record.this);
+                    getActivity().stopService(intent);
                     arrayAdapter.notifyDataSetChanged();
 
                 }
@@ -134,50 +159,4 @@ public class fragment_record extends Fragment implements SensorEventListener {
         return rootView;
     }
 
-
-
-    @Override
-    public void onSensorChanged(SensorEvent sensorEvent) {
-
-        Sensor mySensor = sensorEvent.sensor;
-
-        if (mySensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-            float x = sensorEvent.values[0];
-            float y = sensorEvent.values[1];
-            float z = sensorEvent.values[2];
-
-            long curTime = System.currentTimeMillis();
-
-            if ((curTime - lastUpdate) > 100) {
-                long diffTime = (curTime - lastUpdate);
-                lastUpdate = curTime;
-
-                float speed = Math.abs(x + y + z - last_x - last_y - last_z)/ diffTime * 10000;
-
-                if (speed > SHAKE_THRESHOLD) {
-
-                }
-
-                last_x = x;
-                last_y = y;
-                last_z = z;
-            }
-        }
-
-    }
-
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
-    }
-
-    public void onPause() {
-        super.onPause();
-        senSensorManager.unregisterListener(this);
-    }
-
-    public void onResume() {
-        super.onResume();
-        senSensorManager.registerListener(this, senAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
-    }
 }
